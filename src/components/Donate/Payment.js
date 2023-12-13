756d9331617937a8cc4054c198f4efeeca4e4d91
abdulhamiid <adioabdulhamid@yahoo.com>
2023-12-13 09:41:56 +0100

    Toggle payment card style

--- src/components/Donate/Payment.js
+++ src/components/Donate/Payment.js
@@ -17,19 +17,23 @@ const toggleDisplay = (e) => {
 
   document.querySelectorAll('button').forEach((button) => {
     if (button.id === id) {
-      button.classList.add('bg-[#007300]');
       button.classList.remove('bg-white');
+      button.classList.add('bg-[#007300]');
+      button.childNodes[0].classList.remove('bg-[#007300]');
       button.childNodes[0].classList.add('bg-white');
       button.childNodes[0].classList.remove('text-white');
       button.childNodes[0].classList.add('text-[#007300]');
+      button.childNodes[1].classList.remove('text-[#007300]');
       button.childNodes[1].classList.add('text-white');
     } else {
-      button.classList.add('bg-white');
       button.classList.remove('bg-[#007300]');
+      button.classList.add('bg-white');
       button.childNodes[0].classList.remove('bg-white');
+      button.childNodes[0].classList.add('bg-[#007300]');
       button.childNodes[0].classList.remove('text-[#007300]');
       button.childNodes[0].classList.add('text-white');
       button.childNodes[1].classList.remove('text-white');
+      button.childNodes[1].classList.add('text-[#007300]');
     }
   });
 };
@@ -61,11 +65,11 @@ function Payment() {
         {
           data.map((item) => (
             <li key={item.id} id={item.id} className="li w-full">
-              <button id={item.id} type="button" className={`button flex flex-col items-center bg-white justify-center py-14 ${item.id === 1 ? 'rounded-tr-[34px]' : ''} border-b-2 border-[#007300]-600 w-full`} onClick={(e) => toggleDisplay(e)}>
-                <div id={item.id} className="border border-[#007300] rounded-full p-3 bg-[#007300] text-white">
+              <button id={item.id} type="button" className={`button flex flex-col items-center justify-center py-14 ${item.id === 1 ? 'rounded-tr-[34px] bg-[#007300]' : 'bg-white'} border-b-2 border-[#007300]-600 w-full`} onClick={(e) => toggleDisplay(e)}>
+                <div id={item.id} className={`border border-[#007300] rounded-full p-3 ${item.id === 1 ? 'bg-white text-[#007300]' : 'bg-[#007300] text-white'}`}>
                   {item.icon}
                 </div>
-                <p id={item.id} className="text-[#007300] text-base">{item.text}</p>
+                <p id={item.id} className={`${item.id === 1 ? 'text-white' : 'text-[#007300]'} text-base`}>{item.text}</p>
               </button>
             </li>
           ))
