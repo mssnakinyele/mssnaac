3ab8539a9aedce0c2b854ca675dfa6ddb9a8a681
Abdulrasheed <yinka.jsx@gmail.com>
2023-12-28 13:05:36 +0100

    refactored code base and added an error page

---
+++ src/views/index.js
@@ -0,0 +1,9 @@
+import Home from './Home/Home';
+import About from './About/About';
+import Donate from './Donate/Donate';
+import Detail from './Donate/Detail';
+import Error404 from './Error/Error404';
+
+import MainLayout from '../layouts/MainLayout';
+
+export { Home, About, Donate, Detail, MainLayout, Error404 };
